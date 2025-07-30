a0ec1e99cf4056612466990c57cd1cb571db778b
Samiuk <lahlahhoussam80@gmail.com>
2025-07-31 00:10:00 +0100

    add 'createLookup' function scaffold and test placeholder - Created 'createLookup' function file with module export. - Added a test.todo placeholder for creating country-currency code lookup functionality. - Setup initial structure for implementation and testing.

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,15 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  const input = [['US', 'USD'], ['CA', 'CAD'], ['GB', 'GBP']];
+  const expected = {
+    US: 'USD',
+    CA: 'CAD',
+    GB: 'GBP',
+  };
+  expect(createLookup(input)).toEqual(expected);
+});
+
 
 /*
 
