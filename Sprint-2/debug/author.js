0b3bd51a3609d73067436756d480624b0189adae
Samiuk <lahlahhoussam80@gmail.com>
2025-07-30 23:21:28 +0100

    iterate over object values correctly to log all properties - Replaced invalid for...of loop on object with Object.values() - Ensures all property values of author are logged without errors

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+// The code attempts to loop over author using for...of.
+// But for...of works on iterable objects like arrays, strings, Maps, Sets.
+// Plain objects ({}) are NOT iterable, so for (const value of author) will throw a TypeError:
+// author is not iterable.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +15,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+for (const key in author) {
+  console.log(author[key]);
 }
