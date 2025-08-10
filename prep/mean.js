36b8098098643d628d16591bbf82b931bae78765
Samiuk <lahlahhoussam80@gmail.com>
2025-08-10 14:37:10 +0100

    Remove prep folder from clean branch

--- prep/mean.js
+++
@@ -1,9 +0,0 @@
-function calculateMean(list) {
-  const sum = list.reduce((acc, num) => acc + num, 0);
-  return sum / list.length;
-}
-
-module.exports = calculateMean;
-
-
-// module.exports = { calculateMean };
--- prep/parse-query-string.test.js
+++
@@ -1,7 +0,0 @@
-test("given a query string with multiple key-value pairs, returns them in object form", function () {
-  const input = "sort=lowest&colour=yellow";
-  const currentOutput = parseQueryString(input);
-  const targetOutput = { sort: "lowest", colour: "yellow" };
-
-  expect(currentOutput).toEqual(targetOutput);
-});
